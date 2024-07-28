b5b3ed011452474fc634fb79f64521d5261cc777
supmbh <supmbh@gmail.com>
2024-07-28 15:38:57 +0200

    debug debut

--- src/components/ScoreChart/ScoreChart.jsx
+++ src/components/ScoreChart/ScoreChart.jsx
@@ -1,25 +1,45 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { AllData } from "../../service/allData";
-import ScoreChartComponent from "./ScoreChartComponent";
+import { RadialBarChart, RadialBar, PolarAngleAxis, ResponsiveContainer } from "recharts";
+import PropTypes from 'prop-types'
+import style from '../ScoreChart/ScoreChart.module.css'
 
-const ScoreChart = () => {
-    const { userId } = useParams();
-    const [data, setData] = useState(null);
+/**
+ * displays the user's score
+ * @param {object} {data}
+ * @returns {JSX.Element}
+ */
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const calling = await AllData("UserScore", parseInt(userId));
-                setData(calling.data.todayScore || calling.data.score);
-            } catch (error) {
-                console.error('Erreur lors de la récupération des données de score', error);
-            }
-        };
-        fetchData();
-    }, [userId]);
+const ScoreChart = ({data}) => {
 
-    return data !== null ? <ScoreChartComponent data={data} /> : <p>Données de score non disponibles</p>;
-};
+    const result = data * 100
+    const score = [
+        { value: result, fill: "#FF0000" },
+    ]
 
-export default ScoreChart;
+    return (
+        <div className={style.scoreContainer}>
+            <h2>Score</h2>
+            <ResponsiveContainer width="100%" height="100%">
+                <RadialBarChart data={score} innerRadius={85} startAngle={90} endAngle={450}>
+                    <PolarAngleAxis type="number" domain={[0, 100]} tick={false} />
+                    <RadialBar dataKey="value" cornerRadius="100%" />
+                </RadialBarChart>
+            </ResponsiveContainer>
+            <div className={style.scoreText}>
+                <p>
+                    {result}%
+                    <br />
+                    de votre
+                    <br />
+                    objectif
+                </p>
+            </div>
+        </div>
+    )
+
+}
+
+ScoreChart.propTypes = {
+    score: PropTypes.object,
+}
+
+export default ScoreChart
