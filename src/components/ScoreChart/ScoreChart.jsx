f3f5180fef001a1247171a5e846319c2316fae82
supmbh <supmbh@gmail.com>
2024-07-28 16:02:07 +0200

    debug Scorechart pur (les props étaient déjà passés par index page

--- src/components/ScoreChart/ScoreChart.jsx
+++ src/components/ScoreChart/ScoreChart.jsx
@@ -1,45 +1,20 @@
-import { RadialBarChart, RadialBar, PolarAngleAxis, ResponsiveContainer } from "recharts";
-import PropTypes from 'prop-types'
-import style from '../ScoreChart/ScoreChart.module.css'
+import PropTypes from 'prop-types';
+import ScoreChartComponent from "./ScoreChartComponent";
 
 /**
  * displays the user's score
- * @param {object} {data}
+ * @param {object} { data }
  * @returns {JSX.Element}
  */
+const ScoreChart = ({ data }) => {
+    if (data === null) return <p>Données de score non disponibles</p>;
 
-const ScoreChart = ({data}) => {
-
-    const result = data * 100
-    const score = [
-        { value: result, fill: "#FF0000" },
-    ]
-
-    return (
-        <div className={style.scoreContainer}>
-            <h2>Score</h2>
-            <ResponsiveContainer width="100%" height="100%">
-                <RadialBarChart data={score} innerRadius={85} startAngle={90} endAngle={450}>
-                    <PolarAngleAxis type="number" domain={[0, 100]} tick={false} />
-                    <RadialBar dataKey="value" cornerRadius="100%" />
-                </RadialBarChart>
-            </ResponsiveContainer>
-            <div className={style.scoreText}>
-                <p>
-                    {result}%
-                    <br />
-                    de votre
-                    <br />
-                    objectif
-                </p>
-            </div>
-        </div>
-    )
-
-}
+    return <ScoreChartComponent data={data} />;
+};
 
 ScoreChart.propTypes = {
-    score: PropTypes.object,
-}
+    data: PropTypes.number.isRequired,
+};
 
-export default ScoreChart
+export default ScoreChart;
+
